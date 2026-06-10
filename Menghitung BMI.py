7441c00b228409216b0e328c24e08b4be0854a97
Karisma Vinda Nissa K. <132274279+karsvin26@users.noreply.github.com>
2026-06-10 16:58:20 +0700

    Create Menghitung BMI.py

---
+++ Menghitung BMI.py
@@ -0,0 +1,6 @@
+#Menghitung BMI
+
+berat = float(input("Masukan berat Anda dalam kg: "))
+tinggi = float(input("Masukan tinggi Anda dalam m: "))
+bmi = (berat/(tinggi**2))
+print("Berikut hasil BMI Anda: ", bmi)
